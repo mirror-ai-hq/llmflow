85e19168136bff9d6c179eba3144a24599b57ee5
John Childs-Eddy <johnchildseddy@MacBook-Pro-2.local>
2024-10-01 09:51:07 +1000

    changed type inference

--- src/llm/llm-flow.ts
+++ src/llm/llm-flow.ts
@@ -3,7 +3,7 @@ import { resolveLLM } from "./llm-resolver";
 import { ParsingService } from "./common/utils/parsing.service";
 import {
   PromptTemplate,
-  ValidateTemplateVariables,
+  StrictInputType,
   createPromptTemplate,
   formatPrompt,
 } from "./prompt-template";
@@ -16,10 +16,10 @@ interface VersioningOptions {
   storePath: string;
 }
 
-interface PromptVersion<Template extends string> {
+interface PromptVersion {
   id: string;
   timestamp: number;
-  template: Template;
+  template: string;
   options: LLMOptions;
 }
 
@@ -27,11 +27,7 @@ function generateUUID(): string {
   return crypto.randomUUID();
 }
 
-export class LLMFlow<
-  Template extends string,
-  Input extends Record<string, any>,
-  Output = string
-> {
+export class LLMFlow<Template extends string, TOutput = string> {
   private llmPromise: Promise<LLM>;
   private parser: ParsingService;
   private versionId: string | null = null;
@@ -39,7 +35,7 @@ export class LLMFlow<
   private promptTemplate: PromptTemplate<Template>;
 
   constructor(
-    template: Template,
+    private template: Template,
     private options: LLMOptions,
     versioningOptions?: Partial<VersioningOptions>
   ) {
@@ -48,6 +44,7 @@ export class LLMFlow<
     }
     this.llmPromise = resolveLLM(options.model);
     this.parser = new ParsingService();
+    this.promptTemplate = createPromptTemplate(template);
     this.versioningOptions = {
       versioningEnabled: versioningOptions?.versioningEnabled ?? false,
       storePath: versioningOptions?.storePath ?? "./prompt-versions",
@@ -55,10 +52,9 @@ export class LLMFlow<
     if (this.versioningOptions.versioningEnabled) {
       this.versionId = generateUUID();
     }
-    this.promptTemplate = createPromptTemplate(template);
   }
 
-  async run(input: Input): Promise<Output> {
+  async run(input: StrictInputType<Template>): Promise<TOutput> {
     const llm = await this.llmPromise;
     const prompt = formatPrompt(this.promptTemplate, input);
     const response = await llm.execute(prompt, this.options);
@@ -68,29 +64,29 @@ export class LLMFlow<
     }
 
     if (this.options.dontParse) {
-      return response as Output;
+      return response as TOutput;
     }
 
     if (typeof response === "string") {
       const cleanedResponse = this.parser.cleanMarkdown(response);
       try {
         const extractedJson = this.parser.extractJsonFromText(cleanedResponse);
-        return JSON.parse(extractedJson) as Output;
+        return JSON.parse(extractedJson) as TOutput;
       } catch (error) {
-        return cleanedResponse as unknown as Output;
+        return cleanedResponse as unknown as TOutput;
       }
     }
 
-    return response as Output;
+    return response as TOutput;
   }
 
   private async saveVersion(): Promise<void> {
     if (!this.versionId) return;
 
-    const version: PromptVersion<Template> = {
+    const version: PromptVersion = {
       id: this.versionId,
       timestamp: Date.now(),
-      template: this.promptTemplate.template,
+      template: this.template,
       options: this.options,
     };
 
@@ -103,214 +99,10 @@ export class LLMFlow<
   }
 }
 
-export function createLLMFlow<Input extends Record<string, any>>() {
-  return function <Template extends string>(
-    template: ValidateTemplateVariables<Template, Input>,
-    options: LLMOptions,
-    versioningOptions?: Partial<VersioningOptions>
-  ): LLMFlow<Template, Input> {
-    return new LLMFlow<Template, Input>(template, options, versioningOptions);
-  };
+export function createLLMFlow<Template extends string, TOutput = string>(
+  template: Template,
+  options: LLMOptions,
+  versioningOptions?: Partial<VersioningOptions>
+): LLMFlow<Template, TOutput> {
+  return new LLMFlow<Template, TOutput>(template, options, versioningOptions);
 }
-
-// function returnLLMFlow<
-//   Template extends string,
-//   Input extends Record<string, any>
-// >(
-//   template: ValidateTemplateVariables<Template, Input>,
-//   options: LLMOptions,
-//   versioningOptions?: Partial<VersioningOptions>
-// ): LLMFlow<Template, Input> {
-//   return new LLMFlow<Template, Input>(template, options, versioningOptions);
-// }
-
-// export function createLLMFlow<Input extends Record<string, any>>() {
-//   return function <Template extends string>(
-//     template: ValidateTemplateVariables<Template, Input>,
-//     options: LLMOptions,
-//     versioningOptions?: Partial<VersioningOptions>
-//   ): LLMFlow<Template, Input> {
-//     return returnLLMFlow<Template, Input>(template, options, versioningOptions);
-//   };
-// }
-
-// export class LLMFlow<Template extends string, TOutput = string> {
-//   private llmPromise: Promise<LLM>;
-//   private parser: ParsingService;
-//   private versionId: string | null = null;
-//   private versioningOptions: VersioningOptions;
-
-//   constructor(
-//     private promptTemplate: PromptTemplate<Template>,
-//     private options: LLMOptions,
-//     versioningOptions?: Partial<VersioningOptions>
-//   ) {
-//     if (!options.model) {
-//       throw new Error("Model not specified in LLM options.");
-//     }
-//     this.llmPromise = resolveLLM(options.model);
-//     this.parser = new ParsingService();
-//     this.versioningOptions = {
-//       versioningEnabled: versioningOptions?.versioningEnabled ?? false,
-//       storePath: versioningOptions?.storePath ?? "./prompt-versions",
-//     };
-//     if (this.versioningOptions.versioningEnabled) {
-//       this.versionId = generateUUID();
-//     }
-//   }
-
-//   async run<TInput extends Record<string, unknown>>(
-//     input: ValidateTemplate<Template, TInput>
-//   ): Promise<TOutput> {
-//     const llm = await this.llmPromise;
-//     const prompt = formatPrompt(this.promptTemplate, input);
-//     const response = await llm.execute(prompt, this.options);
-
-//     if (this.versioningOptions.versioningEnabled) {
-//       await this.saveVersion();
-//     }
-
-//     if (this.options.dontParse) {
-//       return response as TOutput;
-//     }
-
-//     if (typeof response === "string") {
-//       const cleanedResponse = this.parser.cleanMarkdown(response);
-//       try {
-//         const extractedJson = this.parser.extractJsonFromText(cleanedResponse);
-//         return JSON.parse(extractedJson) as TOutput;
-//       } catch (error) {
-//         return cleanedResponse as unknown as TOutput;
-//       }
-//     }
-
-//     return response as TOutput;
-//   }
-
-//   private async saveVersion(): Promise<void> {
-//     if (!this.versionId) return;
-
-//     const version: PromptVersion<Template> = {
-//       id: this.versionId,
-//       timestamp: Date.now(),
-//       template: this.promptTemplate.template,
-//       options: this.options,
-//     };
-
-//     const versionPath = path.join(
-//       this.versioningOptions.storePath,
-//       `${this.versionId}.json`
-//     );
-//     await fs.mkdir(this.versioningOptions.storePath, { recursive: true });
-//     await fs.writeFile(versionPath, JSON.stringify(version, null, 2));
-//   }
-// }
-
-// export function createLLMFlow<Template extends string, TOutput = string>(
-//   template: Template,
-//   options: LLMOptions,
-//   versioningOptions?: Partial<VersioningOptions>
-// ): LLMFlow<Template, TOutput> {
-//   const promptTemplate = createPromptTemplate(template);
-//   return new LLMFlow<Template, TOutput>(
-//     promptTemplate,
-//     options,
-//     versioningOptions
-//   );
-// }
-
-// interface VersioningOptions {
-//   versioningEnabled: boolean;
-//   storePath: string;
-// }
-
-// interface PromptVersion {
-//   id: string;
-//   timestamp: number;
-//   template: string;
-//   options: LLMOptions;
-// }
-
-// function generateUUID(): string {
-//   return crypto.randomUUID();
-// }
-
-// export class LLMFlow<V extends Record<string, any>, TOutput = string> {
-//   private llmPromise: Promise<LLM>;
-//   private parser: ParsingService;
-//   private versionId: string | null = null;
-//   private versioningOptions: VersioningOptions;
-//   private promptTemplate: PromptTemplate<string>;
-
-//   constructor(
-//     private template: string,
-//     private options: LLMOptions,
-//     versioningOptions?: Partial<VersioningOptions>
-//   ) {
-//     if (!options.model) {
-//       throw new Error("Model not specified in LLM options.");
-//     }
-//     this.llmPromise = resolveLLM(options.model);
-//     this.parser = new ParsingService();
-//     this.promptTemplate = createPromptTemplate(template);
-//     this.versioningOptions = {
-//       versioningEnabled: versioningOptions?.versioningEnabled ?? false,
-//       storePath: versioningOptions?.storePath ?? "./prompt-versions",
-//     };
-//     if (this.versioningOptions.versioningEnabled) {
-//       this.versionId = generateUUID();
-//     }
-//   }
-
-//   async run(input: V): Promise<TOutput> {
-//     const llm = await this.llmPromise;
-//     const prompt = formatPrompt(this.promptTemplate, input);
-//     const response = await llm.execute(prompt, this.options);
-
-//     if (this.versioningOptions.versioningEnabled) {
-//       await this.saveVersion();
-//     }
-
-//     if (this.options.dontParse) {
-//       return response as TOutput;
-//     }
-
-//     if (typeof response === "string") {
-//       const cleanedResponse = this.parser.cleanMarkdown(response);
-//       try {
-//         const extractedJson = this.parser.extractJsonFromText(cleanedResponse);
-//         return JSON.parse(extractedJson) as TOutput;
-//       } catch (error) {
-//         return cleanedResponse as unknown as TOutput;
-//       }
-//     }
-
-//     return response as TOutput;
-//   }
-
-//   private async saveVersion(): Promise<void> {
-//     if (!this.versionId) return;
-
-//     const version: PromptVersion = {
-//       id: this.versionId,
-//       timestamp: Date.now(),
-//       template: this.template,
-//       options: this.options,
-//     };
-
-//     const versionPath = path.join(
-//       this.versioningOptions.storePath,
-//       `${this.versionId}.json`
-//     );
-//     await fs.mkdir(this.versioningOptions.storePath, { recursive: true });
-//     await fs.writeFile(versionPath, JSON.stringify(version, null, 2));
-//   }
-// }
-
-// export function createLLMFlow<V extends Record<string, any>, TOutput = string>(
-//   template: string,
-//   options: LLMOptions,
-//   versioningOptions?: Partial<VersioningOptions>
-// ): LLMFlow<V, TOutput> {
-//   return new LLMFlow<V, TOutput>(template, options, versioningOptions);
-// }
--- src/llm/prompt-template.ts
+++ src/llm/prompt-template.ts
@@ -1,26 +1,18 @@
-type ExtractVariables<T extends string> =
+export type ExtractVariables<T extends string> =
   T extends `${string}{{${infer Var}}}${infer Rest}`
     ? Var | ExtractVariables<Rest>
     : never;
 
-export type ValidateTemplateVariables<
-  Template extends string,
-  Input extends Record<string, any>
-> =
-  // Check if there are any variables in the template that are not in Input
-  Exclude<ExtractVariables<Template>, keyof Input> extends never
-    ? // Now check if there are any keys in Input that are not used in the template
-      Exclude<keyof Input, ExtractVariables<Template>> extends never
-      ? Template // All variables match; return the Template type
-      : never // Extra keys in Input not used in Template; cause error
-    : never; // Variables in Template not present in Input; cause error
-
-type PromptTemplateType<Template extends string> = {
-  [K in ExtractVariables<Template>]: any;
+export type InferredInputType<T extends string> = {
+  [K in ExtractVariables<T>]: unknown;
 };
 
-export type PromptTemplate<Template extends string> = {
-  template: Template;
+export type StrictInputType<T extends string> = {
+  [K in ExtractVariables<T>]: unknown;
+};
+
+export type PromptTemplate<T extends string> = {
+  template: T;
 };
 
 export function createPromptTemplate<T extends string>(
@@ -29,104 +21,11 @@ export function createPromptTemplate<T extends string>(
   return { template };
 }
 
-export function formatPrompt<T extends string, V extends PromptTemplateType<T>>(
+export function formatPrompt<T extends string>(
   promptTemplate: PromptTemplate<T>,
-  input: V
+  input: StrictInputType<T>
 ): string {
-  return promptTemplate.template.replace(/{{([\w.]+)}}/g, (_, key) => {
-    const keys = key.split(".");
-    let value: any = input;
-    for (const k of keys) {
-      if (value === undefined) break;
-      value = value[k];
-    }
-    return value !== undefined ? String(value) : "";
+  return promptTemplate.template.replace(/{{(\w+)}}/g, (_, key) => {
+    return String((input as any)[key] ?? "");
   });
 }
-
-// ---- this version uses TypeScript generics to enforce the correct input object properties----
-
-// type ExtractVariables<T extends string> =
-//   T extends `${string}{{${infer Var}}}${infer Rest}`
-//     ? Var | ExtractVariables<Rest>
-//     : never;
-
-// type PromptTemplateType<T extends string> = {
-//   [K in ExtractVariables<T>]: any | any[] | Record<string, any>;
-// };
-
-// export type PromptTemplate<T extends string> = {
-//   template: T;
-//   inputVariables: Array<keyof PromptTemplateType<T>>;
-// };
-
-// export function createPromptTemplate<T extends string>(
-//   template: T
-// ): PromptTemplate<T> {
-//   const inputVariables = [...template.matchAll(/{{([\w.]+)(\[\])?}}/g)].map(
-//     (match) => match[1].split(".")[0]
-//   ) as Array<keyof PromptTemplateType<T>>;
-//   return { template, inputVariables };
-// }
-
-// export function formatPrompt<T extends string, V extends PromptTemplateType<T>>(
-//   promptTemplate: PromptTemplate<T>,
-//   input: V
-// ): string {
-//   return promptTemplate.template.replace(
-//     /{{([\w.]+)(\[\])?}}/g,
-//     (_, key, isArray) => {
-//       const keys = key.split(".");
-//       let value = input;
-//       for (const k of keys) {
-//         if (value === undefined) break;
-//         value = value[k as keyof typeof value];
-//       }
-//       if (isArray && Array.isArray(value)) {
-//         return value.join(", ");
-//       }
-//       return value !== undefined ? String(value) : "";
-//     }
-//   );
-// }
-
-// ---- This version finds missing properties in the input object----
-
-// type ExtractVariables<T extends string> =
-//   T extends `${string}{{${infer Var}}}${infer Rest}`
-//     ? Var | ExtractVariables<Rest>
-//     : never;
-
-// type ErrorMessage<T extends string> = `Missing required properties: ${T}`;
-
-// export type ValidateTemplate<
-//   Template extends string,
-//   Input extends Record<string, unknown>
-// > = ExtractVariables<Template> extends keyof Input
-//   ? keyof Input extends ExtractVariables<Template>
-//     ? Input
-//     : ErrorMessage<Exclude<ExtractVariables<Template>, keyof Input> & string>
-//   : ErrorMessage<Exclude<ExtractVariables<Template>, keyof Input> & string>;
-
-// export type PromptTemplate<Template extends string> = {
-//   template: Template;
-// };
-
-// export function createPromptTemplate<Template extends string>(
-//   template: Template
-// ): PromptTemplate<Template> {
-//   return { template };
-// }
-
-// export function formatPrompt<
-//   Template extends string,
-//   TInput extends Record<string, unknown>
-// >(
-//   promptTemplate: PromptTemplate<Template>,
-//   input: ValidateTemplate<Template, TInput>
-// ): string {
-//   return promptTemplate.template.replace(/{(\w+)}/g, (_, key) => {
-//     const typedKey = key as Extract<keyof TInput, string>;
-//     return String((input as TInput)[typedKey]) || "";
-//   });
-// }
